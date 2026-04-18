49811bedb69a855e5677d435db786e3e23517767
KANISHGA <168720129+kanishgask@users.noreply.github.com>
2026-04-18 08:16:37 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import QuestionCard from "./components/QuestionCard";
+
+function App() {
+  return (
+    <div>
+      <h1>AI Interview Assistant</h1>
+      <QuestionCard />
+    </div>
+  );
+}
+
+export default App;
